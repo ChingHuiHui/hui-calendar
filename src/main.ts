85e0e71aa29017f73d8afc97a2a7fed5c239a474
chinghuihui <sunny70624@gmail.com>
2022-06-15 23:51:40 +0800

    task's logic and style

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,63 @@ type DateItem = {
   date: string
 }
 
+const panel = document.querySelector('.panel') as HTMLDivElement
+const panelList = document.querySelector('.panel-list') as HTMLUListElement
+const panelClose = document.querySelector('#panel-close') as HTMLButtonElement
 const days = document.querySelector('.days') as HTMLDivElement
 const title = document.querySelector('.title') as HTMLDivElement
 
 const prev = document.querySelector('#prev') as HTMLButtonElement
 const next = document.querySelector('#next') as HTMLButtonElement
 
-function getNumberOfDaysInAMonth(year: number, month: number) {
+function getNumberOfDaysInAMonth(year: number, month: number): number {
   return new Date(year, month + 1, 0).getDate()
 }
 
-function getDate(date: string) {
+function getDate(date: string): string {
   return new Date(date).getDate().toString()
+}
+
+function fillZero(item: string | number): string {
+  if (typeof item === 'number') {
+    item = `${item}`
+  }
+
+  return item.padStart(2, '0')
+}
+
+function formatDate(dateStr: string): string {
+  const dateObj = new Date(dateStr)
+
+  const year = dateObj.getFullYear()
+  const month = fillZero(dateObj.getMonth() + 1)
+  const date = fillZero(dateObj.getDate())
+
+  return `${year}-${month}-${date}`
+}
+
+type Task = {
+  date: string
+  todo: string[]
+  status: string
+}
+
+class Tasks {
+  constructor(public items: Task[]) {}
+
+  getTasks(date?: string): Task[] {
+    if (!date) {
+      return []
+    }
+
+    return this.items.filter(
+      ({ date: taskDate }) => taskDate === formatDate(date)
+    )
+  }
+
+  haveTask(date?: string) {
+    return this.getTasks(date).length > 0
+  }
 }
 
 class Calendar {
@@ -74,41 +119,41 @@ class Calendar {
     return dates
   }
 
-  #buildDateCells() {
+  #buildDateCells(): void {
     days.innerHTML = ''
     ;[...this.#buildPrevDates(), ...this.#buildCurrentDates()].forEach(
       (dateItem) => {
         const dateCell = document.createElement('time')
-        dateCell.className = 'cell date-cell'
-        dateCell.innerText = getDate(dateItem.date)
-        dateCell.dataset.date = dateItem.date
 
-        if (new Date(dateItem.date).getMonth() !== this.month) {
+        const { date } = dateItem
+
+        dateCell.className = 'cell date-cell'
+        dateCell.innerText = getDate(date)
+        dateCell.dataset.date = date
+
+        if (new Date(date).getMonth() !== this.month) {
           dateCell.classList.add('prev-month')
         }
 
+        if (tasks.haveTask(date)) {
+          const inner = document.createElement('div')
+          inner.classList.add('inner')
+
+          dateCell.appendChild(inner)
+        }
+
         days.appendChild(dateCell)
-
-        const { offsetLeft, offsetTop } =
-          days.children[days.children.length - 1]
-
-        const inner = document.createElement('div')
-        inner.classList.add('inner')
-        inner.style.left = `${offsetLeft}px`
-        inner.style.top = `${offsetTop}px`
-
-        dateCell.appendChild(inner)
       }
     )
   }
 
-  #renderHero() {
-    const displayedMonth = `${this.month + 1}`.padStart(2, '0')
+  #renderHero(): void {
+    const displayedMonth = fillZero(this.month + 1)
 
     title.innerText = `${this.year}.${displayedMonth}`
   }
 
-  prev() {
+  prev(): void {
     let prevMonth = this.month - 1
 
     if (prevMonth < 0) {
@@ -120,7 +165,7 @@ class Calendar {
     this.render()
   }
 
-  next() {
+  next(): void {
     let nextMonth = this.month + 1
 
     if (nextMonth > 11) {
@@ -139,18 +184,6 @@ class Calendar {
   }
 }
 
-const calendar = new Calendar()
-
-calendar.render()
-
-prev.addEventListener('click', () => {
-  calendar.prev()
-})
-
-next.addEventListener('click', () => {
-  calendar.next()
-})
-
 days.addEventListener('click', (e) => {
   const target = e.target as HTMLDivElement
 
@@ -160,14 +193,66 @@ days.addEventListener('click', (e) => {
     child.classList.remove('active')
   })
 
-  if (!target.classList.contains('date-cell')) return
-
-  target.classList.add('active')
-  target.children[0].classList.add('active')
+  if (
+    !target.classList.contains('date-cell') ||
+    !tasks.haveTask(target.dataset.date)
+  ) {
+    return
+  }
 
   calendar.active = target.dataset.date || ''
+
+  panel.classList.add('active')
+  target.classList.add('active')
+
+  const todoList = tasks
+    .getTasks(calendar.activeDate)
+    .map((task) => task.todo.map((todo) => `<li> ${todo}</li>`).join(''))
+
+  const list = `<ul>${todoList}</ul>`
+
+  panelList.innerHTML = list
+})
+
+days.addEventListener('mouseover', (e) => {
+  const target = e.target as HTMLDivElement
+
+  if (!target.classList.contains('date-cell')) return
+
+  if (tasks.haveTask(target.dataset.date)) {
+    target.classList.add('hover:border-blue-400', 'cursor-pointer')
+  }
+
+  const { offsetLeft, offsetTop } = target as HTMLDivElement
+
+  panel.style.left = `${offsetLeft}px`
+  panel.style.top = `${offsetTop}px`
+})
+
+panelClose.addEventListener('click', () => {
+  panel.classList.remove('active')
+})
+
+prev.addEventListener('click', () => {
+  calendar.prev()
+})
+
+next.addEventListener('click', () => {
+  calendar.next()
 })
 
 window.addEventListener('resize', () => {
   calendar.render()
 })
+
+const tasks = new Tasks([
+  {
+    date: '2022-06-12',
+    todo: ['Have Meeting', 'develop a project'],
+    status: 'warning',
+  },
+])
+
+const calendar = new Calendar()
+
+calendar.render()
